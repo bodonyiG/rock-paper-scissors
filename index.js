78e585fefccf1e064b171274b9401dcbbd39ecc1
bodonyiG <bodonyi.gy@gmail.com>
2020-10-03 22:26:42 +0200

    first site and some code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert("help")
+const options = ["rock", "paper", "scissors", "lizard", "spock"];
+let playerSelect = "";
+let gameSelect = "";
+
+$(".item").on("click", function(){
+  if($(this).hasClass("rock")){
+    playerSelect = options[0];
+
+  }else if($(this).hasClass("paper")){
+    playerSelect = options[1];
+
+  }else if($(this).hasClass("scissors")){
+    playerSelect = options[2];
+
+  }
+
+  let gameSelect = options[ Math.floor(Math.random() * 3) ];
+  // alert("gameSelect: " + gameSelect + " | playerSelect: " + playerSelect);
+
+  if(gameSelect === playerSelect){
+    alert("You won");
+  }else{
+    alert("you lost");
+  }
+
+  // window.location.href = "score.html";
+
+   playerSelect = "";
+   gameSelect = "";
+});
